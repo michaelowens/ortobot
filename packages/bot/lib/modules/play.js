ef06643dc0ec3368e368e7dc1b948f4b1c158cc0
Michael Owens <michael@owens.nl>
2021-01-18 14:21:19 +0100

    Remove temporary enabled status for PlayModule

--- packages/bot/lib/modules/play.js
+++ packages/bot/lib/modules/play.js
@@ -4,25 +4,9 @@ module.exports = class PlayModule {
     this.events = {
       message: this.onMessage,
     }
-    this.tmpEnabled = false
   }
 
   async onMessage(data) {
-    if (data.isMod) {
-      if (data.message === 'noplayon') {
-        console.log('No !play allowed')
-        this.tmpEnabled = true
-      }
-      if (data.message === 'noplayoff') {
-        console.log('!play allowed')
-        this.tmpEnabled = false
-      }
-    }
-
-    if (!this.tmpEnabled) {
-      return
-    }
-
     if (data.message.match(/^!play/i)) {
       this.chat.client.privmsg(data.channel, '/delete ' + data.messageID)
       return
