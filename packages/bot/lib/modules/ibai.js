29e3d0e5d2ec410d09f764573559026f8317f242
Michael Owens <michael@owens.nl>
2021-08-02 22:12:06 +0200

    ibaimodule bans now

--- packages/bot/lib/modules/ibai.js
+++ packages/bot/lib/modules/ibai.js
@@ -5,27 +5,19 @@ module.exports = class IbaiModule {
     this.events = {
       message: this.onMessage,
     }
-    this.timeouts = {} // TODO: move to database
-    // console.log('Has module?', modules.has('ibaiModule'))
-    // console.log('Has module?', modules.has('something'))
+    this.timeouts = {}
   }
 
   async onMessage(data) {
     if (data.message.match(/^[l|i]ba+[l|i]+$/i)) {
-      if (typeof this.timeouts[data.user] === 'undefined') {
-        this.timeouts[data.user] = 1
-      } else {
-        this.timeouts[data.user] *= 8
-      }
-
       console.log(
-        'Timed out:',
+        'Banned:',
         data.user,
         `(${1 + Math.log(this.timeouts[data.user]) / Math.log(8)}x)`
       )
 
       await this.chat.client
-        .timeout(data.channel, data.user, 60 * this.timeouts[data.user])
+        .ban(data.channel, data.user, 'Ibai spam')
         .catch((e) => console.log('oopsie, could not timeout'))
     }
   }
