ac3b9a74bec33d38ddd714417a93df7d39713828
Michael Owens <michael@owens.nl>
2022-05-20 15:34:46 +0200

    Make ibai module more lenient, ban at 3rd message

--- packages/bot/lib/modules/ibai.js
+++ packages/bot/lib/modules/ibai.js
@@ -4,15 +4,35 @@ module.exports = class IbaiModule {
     this.events = {
       message: this.onMessage,
     }
+    this.timeouts = {}
   }
 
   async onMessage(data) {
     if (data.message.match(/^[l|i]ba+[l|i]+$/i)) {
-      console.log('Banned:', data.user)
+      if (typeof this.timeouts[data.user] === 'undefined') {
+        this.timeouts[data.user] = 1
+      } else {
+        this.timeouts[data.user] += 1
+      }
 
-      await this.chat.client
-        .ban(data.channel, data.user, 'Ibai spam')
-        .catch((e) => console.log('oopsie, could not timeout'))
+      const ban = this.timeouts[data.user] > 2
+      console.log(
+        ban ? 'Banned:' : 'Timed out',
+        data.user,
+        `(${this.timeouts[data.user]}x)`
+      )
+
+      if (ban) {
+        await this.chat.client
+          .ban(data.channel, data.user, 'Ibai spam')
+          .catch((e) => console.log('oopsie, could not ban'))
+
+        delete this.timeouts[data.user]
+      } else {
+        await this.chat.client
+          .timeout(data.channel, data.user, 60 * this.timeouts[data.user])
+          .catch((e) => console.log('oopsie, could not timeout'))
+      }
     }
   }
 }
